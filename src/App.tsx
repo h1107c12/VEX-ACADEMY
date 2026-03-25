acd930ba281be00a8b5014acaffbfb0b05f0d474
h1107c12 <h1107c12@gmail.com>
2026-03-25 16:41:04 +0900

    init: first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import Header from './components/layout/Header'
+import Footer from './components/layout/Footer'
+import HeroSection from './components/sections/HeroSection'
+import RosterSection from './components/sections/RosterSection'
+import AboutSection from './components/sections/AboutSection'
+import CTASection from './components/sections/CTASection'
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <main>
+        <HeroSection />
+        <RosterSection />
+        <AboutSection />
+        <CTASection />
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
