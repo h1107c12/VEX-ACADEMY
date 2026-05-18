4df9dce89c2f78ea06b711365e3f92f840296b82
h1107c12 <h1107c12@gmail.com>
2026-05-18 19:36:52 +0900

    d8

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Header from './components/layout/Header'
 import HeroSection from './components/sections/HeroSection'
 import ProgramSection from './components/sections/ProgramSection'
 import CurriculumSection from './components/sections/CurriculumSection'
-import RosterSection from './components/sections/RosterSection'
+// import RosterSection from './components/sections/RosterSection'
 import AboutSection from './components/sections/AboutSection'
 import CTASection from './components/sections/CTASection'
 
@@ -12,7 +12,7 @@ function App() {
       <Header />
       <main>
         <HeroSection />
-        <RosterSection />
+        {/* <RosterSection /> */}
         <AboutSection />
         <ProgramSection />
         <CurriculumSection />
